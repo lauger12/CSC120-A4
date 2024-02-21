c781324705485ebc1d788dbf683b35c7cf52dfcd
Lucy Auger <157738635+lauger12@users.noreply.github.com>
2024-02-20 21:29:07 -0500

    Choo Choo!

--- Engine.java
+++ Engine.java
@@ -1,3 +1,47 @@
 public class Engine {
 
+    // attributes
+    private double currentFuel;
+    private double maxFuel;
+    private boolean notEmpty;
+
+    // methods
+    public Engine(FuelType other, double maxFuel, double currentFuel) {
+        this.maxFuel = maxFuel;
+        this.currentFuel = 100;
+    }
+
+    /**
+     * refuel engine by setting currentFuel to maxFuel
+     * 
+     * @param currentFuel
+     */
+    public void refuel(double currentFuel) {
+        this.currentFuel = this.maxFuel;
+    }
+
+    /**
+     * Compares balance to a proposed debit to ensure sufficient funds
+     * 
+     * @param currentFuel current fuel
+     * @return notEmpty bool T/F: is fuel > 0?
+     */
+    public boolean go() {
+        notEmpty = true;
+        if (notEmpty) {
+            this.currentFuel -= 1;
+            System.out.println("Current Fuel:" + currentFuel);
+            if (currentFuel == 0.0) {
+                notEmpty = false;}
+        }
+        return notEmpty;
+    }
+
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.STEAM, 100.0, 100.0);
+        while (myEngine.go()) {
+            System.out.println("Choo choo!");
+        }
+        System.out.println("Out of fuel.");
+    }
 }
